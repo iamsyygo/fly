936fb05da0f7fcfe4db6201b2065bf987a8fec2c
李雨锋 <liyufeng@gz-yibo.com>
2025-01-23 10:28:20 +0800

    fix: update pkgShorthandName from 'sp' to 'fly'

--- internal/constant/src/pkg.ts
+++ internal/constant/src/pkg.ts
@@ -1,6 +1,6 @@
 export const pkg = 'fly'
 export const pkgScope = '@fly'
-export const pkgShorthandName = 'sp'
+export const pkgShorthandName = 'fly'
 
 export const esmDir = 'es'
 export const libDir = 'lib'
